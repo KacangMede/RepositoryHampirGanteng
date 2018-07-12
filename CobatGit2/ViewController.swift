d55c9ab6d8ff6591e7ce3fe3f8704a46b40e4b27
Kevin Elbert <kevinelbertsetiawan@gmail.com>
2018-07-12 15:39:46 +0700

    Initial Commit

---
+++ CobatGit2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CobatGit2
+//
+//  Created by Kevin Elbert on 12/07/18.
+//  Copyright © 2018 Kevin Elbert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
